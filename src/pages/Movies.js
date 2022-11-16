73396704da8693f99bda34f3a990c7112c1a3fbb
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-16 10:58:25 +0700

    add search bar movies

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Movies() {
   return (
-    <div>
+    <div className="container-fluid mt-3">
       <h1>Movies</h1>
+      <div class="form-group has-search d-flex justify-content-center">
+        <span class="form-control-feedback">
+          <i class="bi bi-search search-icon"></i>
+        </span>
+        <input
+          type="text"
+          class="form-control bg-transparent w-50 search-text"
+          placeholder="Search"
+        />
+      </div>
     </div>
   );
 }
