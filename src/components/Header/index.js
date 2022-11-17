48a5ee329960f9fc69bb1178a509a106b3b76825
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-11-17 10:57:39 +0700

    fixing navbar header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from "react-bootstrap";
 
 export default function index() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
         <div className="container">
-          <span className="navbar-brand">MyFlix</span>
+          <span className="navbar-brand">Movie Finder</span>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
+            <ul className="navbar-nav mr-auto ml-5">
               <li className="nav-item active">
                 <Link to="/" className="nav-link" href="#">
                   Movies
@@ -21,6 +22,17 @@ export default function index() {
                 </Link>
               </li>
             </ul>
+            <div className="ml-auto">
+              <Button
+                variant="outline-warning"
+                href="https://github.com/fjrpambudhi74/react-movies"
+              >
+                <span>
+                  <i className="bi bi-github mr-2"></i>
+                </span>
+                Github
+              </Button>
+            </div>
           </div>
         </div>
       </nav>
